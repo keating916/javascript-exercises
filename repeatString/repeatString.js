8ef3b90e44422e15c7d3cd2cbcc8696d088e042e
Patrick Keating <keating916@gmail.com>
2019-04-29 16:30:16 -0700

    fix repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let out = ""
+    if(num < 0) {
+        return 'ERROR';
+    }else {
+        for(let i = 0;i < num; i++) {
+            out= `${out}${str}`;
+        };
+    }
+    return out;
 }
 
 module.exports = repeatString
