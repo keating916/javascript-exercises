2d3cdb639d61a52b87c15db35c1d6682572dc99f
Patrick Keating <keating916@gmail.com>
2019-04-30 22:28:37 -0700

    completed all tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    console.log(word.split(""))
+    let i= word.length -1;
+    let array = ""
+    while(i>= 0) {
+        array = array + word[i];
+        i--
+    }
+    return array;
 }
-
 module.exports = reverseString
