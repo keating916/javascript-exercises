b8c73060e9fcb7a8a1d53bb2f57b547d81353b3e
Patrick Keating <keating916@gmail.com>
2019-05-02 20:33:05 -0700

    tempconversion finish

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celc = (temp-32)*(5/9)
+  celc = Math.ceil(celc *10)/10
+  return celc
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let far = temp*(9/5) + 32
+  far = Math.ceil(far *10)/10
+  return far;
 }
 
 module.exports = {
