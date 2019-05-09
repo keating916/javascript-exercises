e7fc0cc1ba889ac40cc490d183417680a7bc9b61
Patrick Keating <keating916@gmail.com>
2019-05-08 21:09:32 -0700

    finish

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,48 @@
-function add () {
-	
+function add (x,y) {
+	return x+y
 }
 
-function subtract () {
-	
+function subtract (x,y) {
+	return x - y
 }
 
-function sum () {
-	
+function sum (array) {
+	let l = array.length;
+	let total = 0
+	if(array.length == 0) {
+		return 0
+	}else {
+		for(let i = 0; i<l;i++) {
+			total += array[i]
+		}
+		return total;
+	}
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let l = array.length;
+	let total
+	if(l >= 2) {
+		total = 1;
+	}else {
+		console.log("ERROR");
+	}
+	for(let i =0; i<l; i++) {
+		total *= array[i]
+	}
+	return total;
 }
 
-function power() {
-	
+function power(x,y) {
+	return x ** y;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let total = 1
+	for(let i = 1; i <= x; i++) {
+		total *= i
+	}
+	return total;
 }
 
 module.exports = {
