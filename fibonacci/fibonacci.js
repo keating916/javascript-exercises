e57639e0d5747d3dda129d95a1298198bed38553
Patrick Keating <keating916@gmail.com>
2019-05-15 21:18:28 -0700

    finish solving fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1,1,2];
+    console.log(n);
+    if(typeof n != Number) {
+        n = parseInt(n);
+    }
+    if(n < 1){return "OOPS"}
+    else if(n < 3){
+        return arr[n-1];
+    }else {
+        for(let i = 3; i <= n; i++) {
+            arr.push(arr[i-1] + arr[i-2])
+        }
+    }
+    return arr[n-1]
 }
 
 module.exports = fibonacci
