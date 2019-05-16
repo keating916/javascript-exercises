d548a280fa7289a58e145dd4fc00621837c949c6
Patrick Keating <keating916@gmail.com>
2019-05-15 22:30:28 -0700

    finish findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    people.map(a => {
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = 2019;
+        }
+        a.age = a.yearOfDeath - a.yearOfBirth})
+    people.sort((a,b) => (a.age - b.age)).reverse()
+    return people[0]
+    ;
 }
 
 module.exports = findTheOldest
