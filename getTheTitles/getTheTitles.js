adad1c8ed7fa95535aa8752179c1cc36b7f8611b
Patrick Keating <keating916@gmail.com>
2019-05-15 21:26:34 -0700

    finish getTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let output = []
+    arr.map(a => {output.push(a.title)})
+    return output;
 }
 
 module.exports = getTheTitles;
